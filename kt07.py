5eae78063704b6adabdcc59393b87de95299fb3c
luksken24 <ken@hkhk.edu.ee>
2025-02-06 11:39:49 +0200

    Update kt07.py

--- kt07.py
+++ kt07.py
@@ -1,14 +1,24 @@
 import tkinter as tk
 
+
+
 def valideeriTeksti(*args):
     text = entry_var.get()
-    if len(text) >= 5:
-        validation_label.config(text="Korras!", fg="green")
+    if len(text) == 11:
+        sugunr = int(text[0])
+        if sugunr%2==0:
+            sugu = "naine"
+        else:
+            sugu = "mees"
+        validation_label.config(text=sugu, fg="green")
     else:
-        validation_label.config(text="Sisesta vähemalt 5 märki!", fg="red")
+        validation_label.config(text="Sisesta vähemalt 11 märki!", fg="red")
 
 aken = tk.Tk()
 aken.title("Validaator")
+aken.geometry("400x400")
+
+label = tk.Label(aken, text="Isikukood", font="Arial 16").pack(side='left')
 
 entry_var = tk.StringVar()
 entry_var.trace_add("write", valideeriTeksti)
